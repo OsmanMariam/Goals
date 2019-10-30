bff59d1798ca30139e102dea22cef2734a3e0415
OsmanMariam <sufimariam8@gmail.com>
2019-10-30 09:48:54 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Goal} from './goal';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title='Goals'
+  //goals:Goal=[
+  //   {id:1,name:"Watch Finding Nemo"},
+  //   {id:2,name:"Buy cookies"},
+  //   {id:3,name:"Get new phone case"},
+  //   {id:4,name:"Get dog food"},
+  //   {id:5,name:"Solve math homework"},
+  //   {id:6,name:"Plot my world domination plan"},
+  // ]
+};
